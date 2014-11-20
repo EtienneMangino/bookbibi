df1e629e9d02c762685132a91f83faec1d0f2128
EtienneMangino <etiennemangino@gmail.com>
2014-11-20 16:01:33 +0100

    version 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,21 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :layout_by_resource
+
+
 
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:last_name, :first_name, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:last_name, :first_name, :email, :password, :password_confirmation) }
+  end
+
+  def layout_by_resource
+    if devise_controller? && action_name == "new"
+      "home_pres"
+    else
+      "application"
+    end
   end
 end
